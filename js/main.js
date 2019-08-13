54127cac8b09c8b6854c95d6e341ae888a669f5e
Mr. Anderson <github@hirter.dev>
2019-08-13 23:18:09 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Bridges from './vue/Bridges.vue'
+
+new Vue({
+    el: '#bridges',
+    render: h => h(Bridges)
+})
---
+++ js/vue/Bridges.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+    <section class="bridges">
+        <div class="phue-text">
+            Es wurden noch keine Hue Bridges eingerichtet.
+        </div>
+        <div class="text-center">
+            <a href="#" class="btn btn-primary"><i class="fas fa-search"></i> Nach Bridges suchen</a>
+        </div>
+    </section>
+</template>
+
+<script>
+
+</script>
